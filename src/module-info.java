a9d8222cded91d2d2f61df28273dc4eec593fe81
nicolastoledo <nicky.96@hotmail.com>
2021-07-26 20:17:48 -0300

    Department service

--- src/module-info.java
+++ src/module-info.java
@@ -4,4 +4,5 @@ module javafxjdbc {
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 }
