18c288a9874baa522a21f9b409c95d4cff3419a9
nicolastoledo <nicky.96@hotmail.com>
2021-07-27 02:29:27 -0300

    Database Access

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module javafxjdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
