105ff8793e21645347b27c534aa164f3c01e45c0
nicolastoledo <nicky.96@hotmail.com>
2021-07-29 18:17:43 -0300

    ValidationException / Update department

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module javafxjdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+	requires java.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
